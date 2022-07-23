40faf4b1235e7c8737b78542ba8c83e9fe4c7ea3
bernardkung <bpkung@gmail.com>
2022-07-22 21:28:10 -0400

    Reset after failed force

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import BarChart from './components/BarChart'
+
+
+async function loadData() {
+  return fetch('data/episodes.json')
+    .then(r=>r.json())
+    .then(data=>data)
+}
 
 function App() {
+  
+  const [episodes, setEpisodes] = useState([])
+
+  // Load Data
+  useEffect(() => {
+    loadData()
+      .then(d=>setEpisodes(d))
+  }, []);
+
+  useEffect(()=>{
+    console.log('ep change')
+    console.log(episodes)
+  }, [episodes])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello World</div>
+      <div>There are {episodes.length} episodes loaded</div>
+      <BarChart />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/BarChart.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import * as d3 from 'd3js';
+
+function BarChart() {
+
+}
+
+export default BarChart
